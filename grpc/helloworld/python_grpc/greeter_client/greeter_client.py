388e85063303f335488446ced392bdc876b94103
luoruofeng <lrf8933@163.com>
2019-12-19 21:01:12 +0800

    sys.path.append()

--- grpc/helloworld/python_grpc/greeter_client/greeter_client.py
+++ grpc/helloworld/python_grpc/greeter_client/greeter_client.py
@@ -1,8 +1,13 @@
-# export PYTHONPATH=/Users/luoruofeng/Desktop/go/grpc/helloworld/python_grpc
 from __future__ import print_function
 import logging
 
 import grpc
+
+import sys
+sys.path.append("/Users/luoruofeng/Desktop/go/grpc/helloworld/python_grpc")
+#  if u don't wanna use above line,u can export PYTHONPATH
+#  export PYTHONPATH=/Users/luoruofeng/Desktop/go/grpc/helloworld/python_grpc
+
 from helloworld.protos import helloworld_pb2
 from helloworld.protos import helloworld_pb2_grpc
 #Add __init__.py file to python dir,this will be become to python model,if u want to run module,please use `python -m xxxx.py`
